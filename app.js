ba9337c3dacf9b4f34442aeb5057b7879bc40496
itshelby <neko.klaus.coder@gmail.com>
2018-05-04 09:29:55 +0300

    :heavy_plus_sign: add koa-router

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use((ctx, next) => {
-  ctx.body = 'Hello!'
-})
+const config = require('config')
+
+const Router = require('koa-router')
+const router = new Router()
+
+router
+  .get('/', (ctx, next) => {
+    ctx.body = 'Hello!'
+  })
+
+app.use(router.routes())
 
 app.listen(config.get('port'))
